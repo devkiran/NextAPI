a2eeb25f555389ac67bb375ea8de0c7940437667
Kiran <kiran@boxyhq.com>
2023-01-07 20:36:50 +0530

    use middleware to authenticate the request

--- pages/api/organizations/index.ts
+++ pages/api/organizations/index.ts
@@ -3,34 +3,40 @@ import z from "zod";
 import { prisma } from "@/lib/prisma";
 import { addOrganizationMember } from "@/lib/server/organization";
 
-const schemaOrganization = z.object({
-  name: z.string().min(1),
-  slug: z.string().min(1),
-  website: z.string().optional(),
-  logo: z.string().optional(),
-});
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const { method } = req;
 
-  switch (method) {
-    case "POST":
-      return handlePOST(req, res);
-    default:
-      res.setHeader("Allow", "POST");
-      res.status(405).json({
-        data: null,
-        error: { message: `Method ${method} Not Allowed` },
-      });
+  try {
+    switch (method) {
+      case "POST":
+        return await handlePOST(req, res);
+      default:
+        res.setHeader("Allow", "POST");
+        throw new Error(`Method ${method} Not Allowed`);
+    }
+  } catch (error: any) {
+    return res.status(400).json({
+      data: null,
+      error: {
+        message: error.message,
+      },
+    });
   }
 }
 
 // Create a new organization
 const handlePOST = async (req: NextApiRequest, res: NextApiResponse) => {
-  const response = schemaOrganization.safeParse(req.body);
+  const schema = z.object({
+    name: z.string().min(1),
+    slug: z.string().min(1),
+    website: z.string().optional(),
+    logo: z.string().optional(),
+  });
+
+  const response = schema.safeParse(req.body);
 
   if (!response.success) {
     return res.status(400).json({
