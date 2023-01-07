f2572b03663c908ac64aa49523b7cd351287e9a0
Kiran <kiran@boxyhq.com>
2023-01-07 20:37:01 +0530

    use middleware to authenticate the request

---
+++ middleware.ts
@@ -0,0 +1,39 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import micromatch from "micromatch";
+import { supabase } from "./lib/supabase";
+
+// Add API routes that don't require authentication
+const unAuthenticatedApiRoutes = ["/api/signin", "/api/signup"];
+
+export async function middleware(req: NextRequest) {
+  const { headers, nextUrl } = req;
+  const { pathname } = nextUrl;
+
+  // Bypass routes that don't require authentication
+  if (micromatch.isMatch(pathname, unAuthenticatedApiRoutes)) {
+    return NextResponse.next();
+  }
+
+  // Authenticate the request
+  const token = headers.get("authorization")?.replace("Bearer ", "");
+
+  const { error } = await supabase.auth.getUser(token);
+
+  // Return an error if the request is not authenticated
+  if (error) {
+    const response = JSON.stringify({ error });
+
+    return new NextResponse(response, {
+      status: 401,
+      headers: { "content-type": "application/json" },
+    });
+  }
+
+  return NextResponse.next();
+}
+
+// Limit the middleware to specific '/api/*' routes
+export const config = {
+  matcher: ["/api/:path*"],
+};
